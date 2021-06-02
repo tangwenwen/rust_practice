59bf8b37e7c1ce6e3be77c79155c31dfeee3b907
wenwneTang <tangwenwen@pointone.tech>
2021-06-02 19:03:02 +0800

    first commit

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+const MAX_POINTS: u32 = 100_000;
+
+fn main() {
+    let mut x = 5;
+    println!("the value of x is : {}", x);
+    x = 6;
+    println!("the value of x is : {}", x);
+
+    println!("const value is :{}", MAX_POINTS);
+
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("the value of y is :{}", y);
+}
